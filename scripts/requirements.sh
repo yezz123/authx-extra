aac2f826b330ce2569659c961d682e5c2f0693a1
Yasser Tahiri <yasserth19@gmail.com>
2024-04-14 05:43:06 +0100

    🔧 Update CI workflow to remove installation of optional requirements

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -5,7 +5,6 @@ refresh-lockfiles() {
   find requirements/ -name '*.txt' ! -name 'all.txt' -type f -delete
   uv pip compile requirements/linting.in -o requirements/linting.txt
   uv pip compile requirements/testing.in -o requirements/testing.txt
-  uv pip compile requirements/optional.in -o requirements/optional.txt
   uv pip compile pyproject.toml -o requirements/pyproject.txt
   uv pip install -r requirements/all.txt
 }
